342ee61713927265670c4a2c8fd070777dd39ce3
Max Nezhevenko <71039203+Maksik-94@users.noreply.github.com>
2020-11-30 19:19:37 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -12,13 +12,3 @@ do {
   alert("Вы сломали алгоритм!");
   alert(`${message} ${total}`);
 }
-
-// let total = 0;
-// let input;
-
-// do {
-//   input = prompt('Введите число!');
-//   total += Number(input);
-// } while (input !== null);
-
-// alert(`Общая сумма чисел равна ${total}`);
