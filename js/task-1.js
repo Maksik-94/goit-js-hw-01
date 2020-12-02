d46efddf8523c20218acc6bcbd5a9b1b2e647cb0
Max Nezhevenko <71039203+Maksik-94@users.noreply.github.com>
2020-12-02 22:53:05 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,6 @@
 const name = "«Генератор защитного поля»";
 let price = 1000;
-const massage1 = `Выбран ${name}, цена за штуку ${price} кредитов`;
-console.log(massage1);
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
 //
 price = 2000;
-const massage2 = `Выбран ${name}, цена за штуку ${price} кредитов`;
-console.log(massage2);
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
